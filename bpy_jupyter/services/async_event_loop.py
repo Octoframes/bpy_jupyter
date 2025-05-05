f734c1d696bde1616d7ed295ac80a8be01fb6c4f
Sofus Albert Høgsbro Rose <sofus@sofusrose.com>
2025-05-05 13:20:29 +0200

    docs: added a lot of documentation and switched to `fake-bpy-module`

--- bpy_jupyter/services/async_event_loop.py
+++ bpy_jupyter/services/async_event_loop.py
@@ -14,6 +14,40 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Manages an `asyncio` event loop, which allows running asynchronous extension code in the main thread of Blender.
+
+## Motivation: Waiting for Godo... Something to Happen
+Blender's Python API is not thread safe, meaning that the main thread must be used to update all ex. properties, UI elements, etc. .
+At the same time, when Python code runs in Blender's main thread, the UI becomes unresponsive until it is finished.
+What to do?
+
+Of course, many salient use cases **require** interacting with the main thread, yet **do not constantly require** the CPU's attention:
+
+- **Network Client**: Waiting for responses from a network server, after making a request.
+	- For instance, a progress bar that responds to updates from a cloud-service ex. a render farm, expensive physics simulation, etc. .
+- **Network Server**: Waiting for clients to connect to us,
+	- For instance, a mini web-service enabling IDE shortcuts that trigger actions within a running Blender.
+- **Input Processing**: Waiting for input from some kind of input device.
+	- For instance, an addon that maps game controller buttons to Blender properties, or MIDI sliders to rig properties.
+- **IPC**: Waiting for status updates from an external process, started via ex. `subprocess` or `multiprocessing`.
+	- For instance, monitoring a computationally heavy external command - or simply switching between light/dark mode in response to theming signals on a Linux system's `dbus`!
+
+By inelegantly slapping an `asyncio` event loop on top of Blender's main thread, any code that spends most of its time "just waiting" can now `await` whatever it needs to, without blocking Blender's main thread.
+
+## Limitations
+It's worth saying: **Concurrency is not parallelism**.
+
+When using this system, _all `async` code still runs in the main thread_.
+If that code uses a lot of CPU processing, then **it will block that main thread and freeze Blender's UI**.
+
+To run expensive code "in the background", one should use the right tools for the job, such as `multiprocessing`.
+The `async` part only comes into play when ex. `await`ing messages from that external process to update the extension's UI.
+
+
+Attributes:
+	EVENT_LOOP_TIMEOUT_SEC: Number of seconds between each iteration of the `asyncio` event loop.
+"""
+
 import asyncio
 
 import bpy
@@ -25,18 +59,28 @@ EVENT_LOOP_TIMEOUT_SEC = 0.001
 
 
 ####################
-# - Globals
-####################
-IS_RUNNING: bool = False
-
-
-####################
 # - Event Loop
 ####################
 @bpy.app.handlers.persistent
 def increment_event_loop() -> float:
+	"""Run one iteration of the `asyncio` event loop.
+
+	## Mechanism
+	The hack at work here is thus: Blender's event loop is asked to increment the `asyncio` event loop "very often".
+
+	Each time `increment_event_loop` is called, all pending (non-`await`ing) tasks will run until they either finish, or reach an `await`.
+	This is the meaning of "one iteration", and this is achieved using `loop.call_soon(loop.stop)`, then `loop.run_forever()`.
+
+	Since the event loop retains its state, and ability to accept tasks, after `loop.stop()`, doing this repeatedly amounts to a frequently invoked "pause and flush".
+
+	Notes:
+		**To enable**, use `bpy.app.timers.register(..., persistent=True)`.
+
+	Returns:
+		The number of seconds to wait before running this function again.
+	"""
 	loop = asyncio.get_event_loop()
-	loop.call_soon(loop.stop)
+	_ = loop.call_soon(loop.stop)
 	loop.run_forever()
 
 	return EVENT_LOOP_TIMEOUT_SEC
@@ -45,13 +89,23 @@ def increment_event_loop() -> float:
 ####################
 # - Start
 ####################
-def start():
-	global IS_RUNNING
-	bpy.app.timers.register(increment_event_loop, persistent=True)
-	IS_RUNNING = True
+def start() -> None:
+	"""Start the `asyncio` event loop.
+
+	Notes:
+		Registers `increment_event_loop` to `bpy.app.timers`.
+
+		**DO NOT** run if an event loop has already been started using `start()`.
+	"""
+	bpy.app.timers.register(increment_event_loop, persistent=True)  # pyright: ignore[reportUnknownMemberType]
 
 
 def stop():
-	global IS_RUNNING
-	bpy.app.timers.unregister(increment_event_loop)
-	IS_RUNNING = False
+	"""Stop a running `asyncio` event loop.
+
+	Notes:
+		Unregisters `increment_event_loop` from `bpy.app.timers`.
+
+		**DO NOT** run if an event loop has not already been started using `start()`.
+	"""
+	bpy.app.timers.unregister(increment_event_loop)  # pyright: ignore[reportUnknownMemberType]
--- bpy_jupyter/services/jupyter_kernel.py
+++ bpy_jupyter/services/jupyter_kernel.py
@@ -38,16 +38,30 @@ from jupyter_client.blocking.client import BlockingKernelClient
 
 
 ####################
-# - Funcs: Utility
+# - External Funcs:
 ####################
-@functools.cache
-def detect_jupyter_py() -> Path:
-	import jupyter
-
-	return Path(jupyter.__spec__.origin).resolve()
-
-
 def ask_remote_kernel_to_shutdown(path_connection_file: Path) -> None:
+	"""Send a shutdown request to a remote kernel, and busy-wait until it has shutdown gracefully.
+
+	Notes:
+		**Must** be called in a different process / "real" thread than that of the running kernel.
+
+	Examples:
+		To stop a kernel running in the current process, one can do something like this:
+		```python
+		path_connection_file = ...  ## Use pathlib.Path
+		p = multiprocessing.Process(
+			target=ask_remote_kernel_to_shutdown,
+			args=(path_connection_file,),
+		)
+		p.start()
+		...  ## Handle requests normally.
+		p.join()  ## Make sure the process closes / any exceptions propagate correctly.
+		```
+
+	Parameters:
+		path_connection_file: The path to the Jupyter kernels's connection file.
+	"""
 	# Create a kernel manager
 	kc = BlockingKernelClient(connection_file=str(path_connection_file))
 	kc.load_connection_file()
@@ -55,8 +69,8 @@ def ask_remote_kernel_to_shutdown(path_connection_file: Path) -> None:
 
 	# Send Shutdown Request to the Kernel
 	kc.shutdown(restart=False)
-	for i in range(1_000):
-		if kc.is_alive():
+	for _ in range(1_000):
+		if not kc.is_alive():
 			break
 
 		time.sleep(0.02)
@@ -69,6 +83,17 @@ def ask_remote_kernel_to_shutdown(path_connection_file: Path) -> None:
 # - Class: IPyKernel
 ####################
 class IPyKernel(pyd.BaseModel):
+	"""Abstraction of the `ipykernel` Jupyter kernel which wraps `ipykernel.kernelapp.IPKernelApp`.
+
+	Attributes:
+		path_connection_file: Path to the connection file to create
+			_`.start()` will overwrite this file._
+
+		_lock: Blocks the use of `_is_running` while `.start()` or `.stop()` are working.
+		_kernel_app: Running embedded `IPKernelApp`, if any is running.
+
+	"""
+
 	path_connection_file: Path
 
 	####################
@@ -77,48 +102,64 @@ class IPyKernel(pyd.BaseModel):
 	_lock: threading.Lock = pyd.PrivateAttr(default_factory=lambda: threading.Lock())
 	_kernel_app: IPKernelApp | None = pyd.PrivateAttr(default=None)
 
-	_is_running: bool = pyd.PrivateAttr(default=False)
-
 	####################
 	# - Properties: Locked
 	####################
 	@functools.cached_property
 	def is_running(self) -> bool:
+		"""Whether this `ipython` Jupyter kernel is currently running."""
 		with self._lock:
-			return self._is_running
+			return self._kernel_app is not None
 
 	####################
 	# - Methods: Lifecycle
 	####################
 	def start(self) -> None:
+		"""Start this `ipykernel` Jupyter kernel.
+
+		Notes:
+			An `asyncio` event loop **must be available**, since the embedded `IPKernelApp` uses this to `await` client requests.
+
+		Raises:
+			ValueError: If an `IPyKernel` is already running.
+		"""
 		with self._lock:
-			if not self._is_running and self._kernel_app is None:
+			if self._kernel_app is None:
 				self._kernel_app = IPKernelApp.instance(
 					connection_file=str(self.path_connection_file)
 				)
 
-				self._kernel_app.initialize([sys.executable])
-				self._kernel_app.kernel.start()
+				self._kernel_app.initialize(  ## pyright: ignore[reportUnknownMemberType]
+					[sys.executable]
+				)
+				self._kernel_app.kernel.start()  ## pyright: ignore[reportUnknownMemberType]
 
-				self._is_running = True
 				with contextlib.suppress(AttributeError):
 					del self.is_running
 
-			elif not self._is_running and self._kernel_app is not None:
-				msg = 'IPyKernel is not running, but has a kernel. This is a bug.'
-				raise RuntimeError(msg)
 			else:
 				msg = "IPyKernel cannot be started, since it's already running."
 				raise ValueError(msg)
 
 	def stop(self) -> None:
-		"""Uses a seperate process to queue a kernel shutdown command via. `jupyter_client`."""
-		with self._lock:
-			if self._is_running and self._kernel_app is not None:
-				self._is_running = False
-				with contextlib.suppress(AttributeError):
-					del self.is_running
+		"""Stop this `ipykernel` Jupyter kernel.
 
+		Notes:
+			An `asyncio` event loop **must be available**, since the embedded `IPKernelApp` uses this to `await` client requests.
+
+			Unfortunately, `IPKernelApp` doesn't provide any kind of `stop()` function.
+			Therefore, there are a LOT of manual hijinks and hacks used in order to cleanly stop and vacuum the running kernel.
+
+		Raises:
+			ValueError: If an `IPyKernel` is not already running.
+			RuntimeErorr: If the `IPKernelApp` doesn't shut down before the timeout.
+
+		See Also:
+			- `ipykernel` Initialization: <https://github.com/ipython/ipykernel/blob/b1283b14419969e36329c1ae957509690126b057/ipykernel/kernelapp.py#L547>
+			- `FIXME` for Kernel Stop in `ipykernel/gui/gtkembed.py`: <https://github.com/ipython/ipykernel/blob/b1283b14419969e36329c1ae957509690126b057/ipykernel/gui/gtkembed.py#L65>
+		"""
+		with self._lock:
+			if self._kernel_app is not None:
 				####################
 				# - Ask Kernel to Shutdown
 				####################
@@ -129,12 +170,13 @@ class IPyKernel(pyd.BaseModel):
 				p.start()
 
 				# Process the Shutdown Request
+				## - Just manually increment the event loop
 				for _ in range(1_000):
 					if not p.is_alive():
 						break
 
 					loop = asyncio.get_event_loop()
-					loop.call_soon(loop.stop)
+					_ = loop.call_soon(loop.stop)
 					loop.run_forever()
 
 					time.sleep(0.02)
@@ -147,15 +189,13 @@ class IPyKernel(pyd.BaseModel):
 				####################
 				# - Manually Close Kernel Resources
 				####################
+				# Close ZMQStream: Control Thread
 				# Manually Close Kernel ZMQStreams
 				## - IPKernelApp.close() doesn't close the kernel streams.
 				## - Until the GC decides to, file-descriptors will remain open.
 				## - Therefore, we can manually close() them
 				## - See <https://github.com/ipython/ipykernel/blob/b1283b14419969e36329c1ae957509690126b057/ipykernel/kernelapp.py#L547>
 				## - See also <https://github.com/zeromq/pyzmq/blob/01bd01c77277f16f714807a3ae4769f5b726710a/zmq/eventloop/zmqstream.py#L508>
-				self._kernel_app.kernel.shell_stream.close()
-				self._kernel_app.kernel.control_stream.close()
-				self._kernel_app.kernel.debugpy_stream.close()
 
 				# Close I/O and Sockets
 				## - Implicitly calls reset_io(), restoring stdout and stderr.
@@ -185,9 +225,9 @@ class IPyKernel(pyd.BaseModel):
 				## TODO: Check refcount?
 				## TODO: Force GC?
 
-			elif self._is_running and self._kernel_app is None:
-				msg = 'IPyKernel is running, but has no kernel. This is a bug.'
-				raise RuntimeError(msg)
+				with contextlib.suppress(AttributeError):
+					del self.is_running
+
 			else:
 				msg = "IPyKernel cannot be stopped, since it's not running."
 				raise ValueError(msg)
